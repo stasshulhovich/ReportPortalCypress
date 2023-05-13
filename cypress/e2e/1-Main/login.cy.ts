8675e143f145c69d9a613350fe9f29ce8ab3f6d6
Stanislau_Shulhovich <Stanislau_Shulhovich@epam.com>
2023-05-13 17:47:51 +0200

    api test done

--- cypress/e2e/1-Main/login.cy.ts
+++ cypress/e2e/1-Main/login.cy.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+/// <reference types="cypress" />>
 
 import { baseUrl } from '../../fixtures/fixt.json';
 import LoginPage from '../../POM/LoginPage';
@@ -8,7 +8,7 @@ describe.only('Test 1: Login to report portal', () => {
     cy.visit(baseUrl);
   });
 
-  it.only('Login to report portal', () => {
+  it('Login to report portal', () => {
     const loginPage = new LoginPage();
     expect(loginPage).not.equal(null);
     const homePage = loginPage.loginToReportPortal();
@@ -16,21 +16,41 @@ describe.only('Test 1: Login to report portal', () => {
     isOpened.should('equal', true);
   });
 
-  it(
-    'smoke test develop api',
-    {
-      env: {
-        api: 'https://dev.myapi.com',
+  it('Api check test', () => {
+    type LaunchModel = {
+      body: {
+        content: {};
+        page: {
+          number: number;
+          size: number;
+          totalElements: number;
+          totalPage: number;
+        };
+      };
+    };
+
+    cy.request({
+      method: 'GET',
+      url: 'api/v1/reportPortal/launch?filter.eq.id=10',
+      headers: {
+        authorization: 'Bearer b2776d0f-f91b-42c5-8eac-b3117dec8f3f',
       },
-    },
-    () => {
-      // This timout applied to this test only
-      Cypress.config('defaultCommandTimeout', 3000);
-      cy.request(Cypress.env('api')).its('status').should('eq', 200);
-    }
-  );
+    }).then((response) => {
+      const jsonString = JSON.stringify(response);
+      const launchModelParsed: LaunchModel = JSON.parse(jsonString);
+      cy.log('The is size ' + launchModelParsed.body.page.size);
+    });
+
+    // cy.request({
+    //   method: 'POST',
+    //   url: 'http://localhost:8080/uat/sso/oauth/token',
+    //   // form: true,
+    //   body: {
+    //     username: 'superadmin',
+    //     password: 'Wtest100*',
+    //   },
+    // });
+  });
 });
 
-/// Implement api
-/// Implement more tests
 /// Read cypress documentation
